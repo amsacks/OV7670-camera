ce99ba2fee4f177b6cffb2fd5388933edefc367c
amsacks <98288814+amsacks@users.noreply.github.com>
2023-02-17 18:24:05 -0800

    Delete sim_scripts/clk_wiz_0 directory

--- sim_scripts/clk_wiz_0/xcelium/run.f
+++
@@ -1,8 +0,0 @@
--makelib xcelium_lib/xil_defaultlib \
-  "../../../../interface_cam.srcs/sources_1/ip/clk_wiz_0/clk_wiz_0_clk_wiz.v" \
-  "../../../../interface_cam.srcs/sources_1/ip/clk_wiz_0/clk_wiz_0.v" \
--endlib
--makelib xcelium_lib/xil_defaultlib \
-  glbl.v
--endlib
-
