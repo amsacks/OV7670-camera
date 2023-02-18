b70aeee8cadd824aa701171010d0c6225054fbf3
amsacks <98288814+amsacks@users.noreply.github.com>
2023-02-17 18:40:59 -0800

    Update debounce.v

--- rtl/debounce.v
+++ rtl/debounce.v
@@ -1,34 +1,46 @@
 `timescale 1ns / 1ps
+`default_nettype none
 
-/* Debounce time calculation: 
+/*
+ *  Filters glitchy inputs by ignoring state changes
+ *  that occur within less than DELAY clock ticks 
+ *
+ *  Registers state change, debounced output, after 
+ *  DELAY number of clock ticks of an unchanged input
+ *  
+ *  NOTE:
+ *  DELAY = (Debounce Time [s]) * (i_clk [Hz])
+ * 
+ */
+
+module debouncer 
+#(parameter DELAY = 1_000_000)
+(
+    input wire  i_clk,
+    input wire  i_btn_in,
+    output wire o_btn_db         
+);
+
+    localparam MAX_COUNT = $clog2(DELAY); 
+    reg [MAX_COUNT-1:0] counter;
+    reg                 r_sample;
     
-    Count_size * (1/f_clk) = Debounce Time
-    Count_size = Debounce Time * f_clk
-*/
-
-module debounce
-    #(  parameter DELAY = 240_000)
-     (  input      i_clk,  
-        input      i_in,      
-        output     o_out
-     );
-     
-    localparam cnt_size = $clog2(DELAY); 
-    reg [cnt_size-1:0] count; 
-    reg                r_sample;
-       
+    initial { counter, r_sample } = 0; 
+    
     always @(posedge i_clk)
         begin
-            if(i_in !== r_sample && count < (DELAY-1))
-                count <= count + 1'b1;
-            else if(count == (DELAY-1))
+            if(i_btn_in !== r_sample && counter < DELAY)
+                counter <= counter + 1'b1; 
+            else if(counter == DELAY)
                 begin
-                    count    <= 0; 
-                    r_sample <= i_in;
-                end 
+                    counter <= 0;
+                    r_sample <= i_btn_in;
+                end
             else
-                count <= 0;  
+                counter <= 0;  
         end 
-   
-   assign o_out = r_sample; 
+
+assign o_btn_db = r_sample; 
+
 endmodule
+
